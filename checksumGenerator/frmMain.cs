97ba00f1d79da6056d24728cfaee566ee3f420da
Gegreenpeaced <107betriebshof@gmail.com>
2022-10-25 09:15:53 +0200

    Update Push: Clipboard Accordance

--- checksumGenerator/frmMain.cs
+++ checksumGenerator/frmMain.cs
@@ -12,39 +12,9 @@ namespace checksumGenerator
             InitializeComponent();
         }
 
-
         private void btnOpenFile_Click(object sender, EventArgs e)
         {
-            // open file dialog
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "All files (*.*)|*.*";
-            ofd.Title = "Select a file";
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                using (MD5 md5 = MD5.Create())
-                {
-                    // read file
-                    byte[] file = System.IO.File.ReadAllBytes(ofd.FileName);
-                    // generate hash
-                    string hash = GetMd5Hash(md5, file);
-                    // display hash
-                    tbMD5Hash.Text = hash;
-                }
-                using (SHA256 sha256 = SHA256.Create())
-                {
-                    // read file
-                    byte[] file = System.IO.File.ReadAllBytes(ofd.FileName);
-                    // generate hash
-                    string hash = Getsha256Hash(sha256, file);
-                    // display hash
-                    tbSha256Hash.Text = hash;
-                }
-            }
-
-
-            /*/
-            // create MD5 hash
-            using (MD5 md5Hash = MD5.Create())
+            try
             {
                 // open file dialog
                 OpenFileDialog ofd = new OpenFileDialog();
@@ -52,15 +22,55 @@ namespace checksumGenerator
                 ofd.Title = "Select a file";
                 if (ofd.ShowDialog() == DialogResult.OK)
                 {
-                    // read file
-                    byte[] file = System.IO.File.ReadAllBytes(ofd.FileName);
-                    // generate hash
-                    string hash = GetMd5Hash(md5Hash, file);
-                    // display hash
-                    tbMD5Hash.Text = hash;
+                    using (MD5 md5 = MD5.Create())
+                    {
+                        // read file
+                        byte[] file = System.IO.File.ReadAllBytes(ofd.FileName);
+                        // generate hash
+                        string md5hash = GetMd5Hash(md5, file);
+                        // display hash
+                        tbMD5Hash.Text = md5hash;
+                    }
+                    using (SHA256 sha256 = SHA256.Create())
+                    {
+                        // read file
+                        byte[] file = System.IO.File.ReadAllBytes(ofd.FileName);
+                        // generate hash
+                        string sha256hash = Getsha256Hash(sha256, file);
+                        // display hash
+                        tbSha256Hash.Text = sha256hash;
+                    }
+                }
+                // Compare Checksums to clipboard
+                if (Clipboard.ContainsText(TextDataFormat.Text))
+                {
+                    {
+                        string clipboardText = Clipboard.GetText(TextDataFormat.Text);
+
+                        if (clipboardText == tbMD5Hash.Text)
+                        {
+                            string message = "MD5-Hash is eqal to clipboard!";
+                            string title = "Accordance found!";
+                            MessageBoxButtons buttons = MessageBoxButtons.OK;
+                            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Information);
+                        }
+                        if (clipboardText == tbSha256Hash.Text)
+                        {
+                            string message = "SHA256-Hash is eqal to clipboard!";
+                            string title = "Accordance found!";
+                            MessageBoxButtons buttons = MessageBoxButtons.OK;
+                            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Information);
+                        }
+                    }
                 }
             }
-            /*/
+            catch(Exception ex)
+            {
+                string message = "Error: " + ex;
+                string title = "Fatal Error!";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Error);
+            }
         }
 
         private string GetMd5Hash(MD5 md5Hash, byte[] file)
@@ -93,7 +103,14 @@ namespace checksumGenerator
 
         private void btnClose_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            string message = "Do you really want to close the application?";
+            string title = "Close Application?";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
         }
     }
 }
