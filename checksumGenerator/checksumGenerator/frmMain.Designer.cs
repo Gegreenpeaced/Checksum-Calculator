6901779d86cc98efdbf21264a7d7ae2ad54767dc
Gegreenpeaced <107betriebshof@gmail.com>
2023-06-08 11:36:33 +0200

    Added Support for SHA1 Checksums

---
+++ checksumGenerator/checksumGenerator/frmMain.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace checksumGenerator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblMD5Checksum = new System.Windows.Forms.Label();
+            this.ofdMD5Checksum = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.tbMD5Hash = new System.Windows.Forms.TextBox();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.lblSha256CheckSum = new System.Windows.Forms.Label();
+            this.tbSha256Hash = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblSha1Checksum = new System.Windows.Forms.Label();
+            this.tbSha1Hash = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMD5Checksum
+            // 
+            this.lblMD5Checksum.AutoSize = true;
+            this.lblMD5Checksum.Location = new System.Drawing.Point(13, 13);
+            this.lblMD5Checksum.Name = "lblMD5Checksum";
+            this.lblMD5Checksum.Size = new System.Drawing.Size(86, 13);
+            this.lblMD5Checksum.TabIndex = 0;
+            this.lblMD5Checksum.Text = "MD5 Checksum:";
+            // 
+            // ofdMD5Checksum
+            // 
+            this.ofdMD5Checksum.FileName = "openFileDialog1";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 104);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Datei öffnen";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // tbMD5Hash
+            // 
+            this.tbMD5Hash.Location = new System.Drawing.Point(122, 10);
+            this.tbMD5Hash.Name = "tbMD5Hash";
+            this.tbMD5Hash.Size = new System.Drawing.Size(491, 20);
+            this.tbMD5Hash.TabIndex = 2;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(93, 104);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(75, 23);
+            this.btnSettings.TabIndex = 3;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // lblSha256CheckSum
+            // 
+            this.lblSha256CheckSum.AutoSize = true;
+            this.lblSha256CheckSum.Location = new System.Drawing.Point(13, 39);
+            this.lblSha256CheckSum.Name = "lblSha256CheckSum";
+            this.lblSha256CheckSum.Size = new System.Drawing.Size(103, 13);
+            this.lblSha256CheckSum.TabIndex = 4;
+            this.lblSha256CheckSum.Text = "SHA256 Checksum:";
+            // 
+            // tbSha256Hash
+            // 
+            this.tbSha256Hash.Location = new System.Drawing.Point(122, 36);
+            this.tbSha256Hash.Name = "tbSha256Hash";
+            this.tbSha256Hash.Size = new System.Drawing.Size(491, 20);
+            this.tbSha256Hash.TabIndex = 5;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(538, 102);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblSha1Checksum
+            // 
+            this.lblSha1Checksum.AutoSize = true;
+            this.lblSha1Checksum.Location = new System.Drawing.Point(13, 65);
+            this.lblSha1Checksum.Name = "lblSha1Checksum";
+            this.lblSha1Checksum.Size = new System.Drawing.Size(91, 13);
+            this.lblSha1Checksum.TabIndex = 7;
+            this.lblSha1Checksum.Text = "SHA1 Checksum:";
+            // 
+            // tbSha1Hash
+            // 
+            this.tbSha1Hash.Location = new System.Drawing.Point(122, 62);
+            this.tbSha1Hash.Name = "tbSha1Hash";
+            this.tbSha1Hash.Size = new System.Drawing.Size(491, 20);
+            this.tbSha1Hash.TabIndex = 8;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 137);
+            this.Controls.Add(this.tbSha1Hash);
+            this.Controls.Add(this.lblSha1Checksum);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.tbSha256Hash);
+            this.Controls.Add(this.lblSha256CheckSum);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.tbMD5Hash);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.lblMD5Checksum);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Hauptformular";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMD5Checksum;
+        private System.Windows.Forms.OpenFileDialog ofdMD5Checksum;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox tbMD5Hash;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Label lblSha256CheckSum;
+        private System.Windows.Forms.TextBox tbSha256Hash;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblSha1Checksum;
+        private System.Windows.Forms.TextBox tbSha1Hash;
+    }
+}
+
---
+++ checksumGenerator/checksumGenerator/frmMain.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace checksumGenerator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public bool multiselect;
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // activate textboxes
+                tbMD5Hash.Enabled = true;
+                tbSha256Hash.Enabled = true;
+                tbSha1Hash.Enabled = true;
+                bool multiselect = false;
+
+                // open file dialog
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Multiselect = true;
+                ofd.Filter = "All files (*.*)|*.*";
+                ofd.Title = "Select a file";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    if (ofd.FileNames.Length == 1)
+                    {
+                        tbMD5Hash.Text = GetMd5Hash(ofd.FileName);
+
+                        tbSha256Hash.Text = Getsha256Hash(ofd.FileName);
+
+                        tbSha1Hash.Text = GetSha1Hash(ofd.FileName);
+
+                    }
+                    else // Begin multiselect files
+                    {
+                        string message = "It looks like you selected multiple files.\nThis program is able to multi-generate checksums of the selected files and dump them into a file\n\nDo you want to continue?";
+                        string title = "Multiselect found!";
+                        MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                        DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+                        if (result == DialogResult.Yes)
+                        {
+                            multiselect = true;
+                            tbMD5Hash.Enabled = false;
+                            tbSha256Hash.Enabled = false;
+                            tbSha1Hash.Enabled= false;
+
+                            // Add all selected files to list
+                            String[] fileNames = ofd.FileNames;
+                            string dumpfilename = "checksumdump.txt";
+
+                            //if (fileNames.Any(dumpfilename.Contains))
+                            //{
+
+                                //create tmp file named "checksumdump.txt"
+                                using (StreamWriter sw = new StreamWriter(dumpfilename))
+                                {
+                                    sw.WriteLine("Filename | MD5 Hash | SHA256 Hash | SHA1 Hash");
+                                    foreach (string fileName in fileNames)
+                                    {
+                                        String md5hash = GetMd5Hash(fileName);
+                                        String sha256hash = Getsha256Hash(fileName);
+                                        String sha1hash = GetSha1Hash(fileName);
+                                        sw.WriteLine($"{fileName} | {md5hash} | {sha256hash} | {sha1hash}");
+                                    }
+                                }
+
+                                // Done message
+                                message = "Done!\n\nDo you want to open the file?";
+                                title = "Task Done!";
+                                buttons = MessageBoxButtons.YesNo;
+                                result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Question);
+                                if (result == DialogResult.Yes)
+                                {
+                                    // open log file with windows notepad
+                                    Process.Start("notepad", dumpfilename);
+                                }
+                                else
+                                {
+                                    // Path message
+                                    message = "The file is located under: " + System.IO.Path.GetDirectoryName(Application.ExecutablePath + dumpfilename); ;
+                                    title = "Information!";
+                                    buttons = MessageBoxButtons.OK;
+                                    result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Information);
+                                }
+                            /*/}
+                            else
+                            {
+                                // Error message string list contains dump file
+                                message = "Your selected files contains the checksum dump file!\n\nUnselect and try again.";
+                                title = "Error!";
+                                buttons = MessageBoxButtons.OK;
+                                result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Error);
+                            }/*/
+                            // clear Textboxes
+                            tbMD5Hash.Text = "";
+                            tbSha256Hash.Text = "";
+                            tbSha1Hash.Text = "";
+                        }
+                    }
+                }
+
+                // no checksum clipboard accordance if multi generation/
+                if (multiselect != true)
+                {
+
+                    // Compare Checksums to clipboard
+                    if (Clipboard.ContainsText(TextDataFormat.Text))
+                    {
+                        {
+                            string clipboardText = Clipboard.GetText(TextDataFormat.Text);
+
+                            if (clipboardText == tbMD5Hash.Text)
+                            {
+                                string message = "MD5-Hash is eqal to clipboard!";
+                                string title = "Accordance found!";
+                                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                                DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Information);
+                            }
+                            if (clipboardText == tbSha256Hash.Text)
+                            {
+                                string message = "SHA256-Hash is eqal to clipboard!";
+                                string title = "Accordance found!";
+                                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                                DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Information);
+                            }
+                            if (clipboardText == tbSha1Hash.Text)
+                            {
+                                string message = "SHA1-Hash is eqal to clipboard!";
+                                string title = "Accordance found!";
+                                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                                DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Information);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string message = "Error: " + ex;
+                string title = "Fatal Error!";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Error);
+            }
+        }
+
+        private string GetMd5Hash(String filename)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] file = System.IO.File.ReadAllBytes(filename);
+                // generate MD5 hash by arguments as string
+                byte[] data = md5.ComputeHash(file);
+                // convert byte array to string
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }
+
+        private string GetSha1Hash(String filename)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                byte[] file = System.IO.File.ReadAllBytes(filename);
+                // generate M5 hash by arguments as string
+                byte[] data = sha1.ComputeHash(file);
+                // convert byte array to string
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }
+
+        private string Getsha256Hash(String filename)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] file = System.IO.File.ReadAllBytes(filename);
+                // generate Sha265 Hash by arguments as string
+                byte[] data = sha256.ComputeHash(file);
+                // convert byte array to string
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            string message = "Do you really want to close the application?";
+            string title = "Close Application?";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
